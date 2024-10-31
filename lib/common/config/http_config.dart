b9948003a6cc2fe2cf1fb10bedf3ad29ef91bd91
Memento-Morii <yafetretta@gmail.com>
2024-10-31 17:56:19 +0300

    fix: code cleanup

--- lib/common/config/http_config.dart
+++ lib/common/config/http_config.dart
@@ -1,5 +1,4 @@
 class HttpConfig {
-  static const String mainDomainName = "api.estota.app";
   static String apiBaseUrl = "https://api.weatherapi.com/v1/";
   static const int defaultConnectionTimeOut = 10 * 1000; //30 sec
   static const int longConnectionTimeOut = 60 * 1000; //60 sec
--- lib/common/theme/color_theme.dart
+++ lib/common/theme/color_theme.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  static Color primaryColor = const Color(0xff792D3D);
-  static Color secondaryColor = const Color(0xff828282);
-  static Color orangeColor = const Color(0xFFF7A116);
-  static Color darkgreyColor = const Color(0xff4f4f4f);
-  static Color whiteErrorColor = const Color(0xFFFFFFFF);
+  static Color whiteColor = const Color(0xFFFFFFFF);
   static Color titleColor = const Color(0xff264653);
   static Color disabledColors = const Color(0xffD3D3D3);
   static Color errorColor = const Color(0xFFdc3545);
